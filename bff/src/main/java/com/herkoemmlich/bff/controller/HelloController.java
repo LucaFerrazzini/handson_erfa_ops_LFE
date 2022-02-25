80280f0b7483155e3380131a904f47c44c90613d
LucaFerrazzini <luca.ferrazzini@ipt.ch>
2022-02-25 16:15:44 +0100

    app-config connection

--- bff/src/main/java/com/herkoemmlich/bff/controller/HelloController.java
+++ bff/src/main/java/com/herkoemmlich/bff/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.herkoemmlich.bff;
+package com.herkoemmlich;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
