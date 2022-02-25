50e973823956c5ada51d66743c7160eb6d730598
LucaFerrazzini <luca.ferrazzini@ipt.ch>
2022-02-25 16:50:52 +0100

    app-config connection

--- bff/src/main/java/com/herkoemmlich/bff/controller/HelloController.java
+++ bff/src/main/java/com/herkoemmlich/bff/controller/HelloController.java
@@ -2,8 +2,10 @@ package com.herkoemmlich.bff;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class HelloController {
     private final MessageProperties properties;
 
